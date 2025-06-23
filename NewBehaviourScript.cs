4ca5f10901082de877ac5669b72afdf35cd5632c
wasori <bonjug33@gmail.com>
2025-06-24 00:19:26 +0900

    변수와 그룹형 변수

--- NewBehaviourScript.cs
+++ NewBehaviourScript.cs
@@ -7,5 +7,46 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         Debug.Log("Hello Unity!");
+
+        ////  1. 변수 ////
+ 
+        int level = 5;
+        float strength = 15.5f;
+        string playerName = "나검사";
+        bool isFullLevel = false;
+
+        Debug.Log("용사의 이름은?");
+        Debug.Log(playerName);
+        Debug.Log("용사의 레벨은?");
+        Debug.Log(level);
+        Debug.Log("용사의 힘은?");
+        Debug.Log(strength);
+        Debug.Log("용사는 만렙인가?");
+        Debug.Log(isFullLevel);
+
+        //// 2. 그룹형 변수 ////
+
+        string[] monsters = { "슬라임", "사막벰", "악마" };
+        int[] monsterLevel = new int[3];
+        monsterLevel[0] = 1;
+        monsterLevel[1] = 6;
+        monsterLevel[2] = 20;
+
+        Debug.Log("맵에 존재하는 몬스터");
+        Debug.Log(monsters[0]);
+        Debug.Log(monsters[1]);
+        Debug.Log(monsters[2]);
+        Debug.Log(monsterLevel[0]);
+        Debug.Log(monsterLevel[1]);
+        Debug.Log(monsterLevel[2]);
+
+        List<string> items = new List<string>();
+        items.Add("생명물약 30");
+        items.Add("마나물약 30");
+
+        items.RemoveAt(0);
+
+        Debug.Log(items[0]);
+        Debug.Log(items[1]);
     }
 }
